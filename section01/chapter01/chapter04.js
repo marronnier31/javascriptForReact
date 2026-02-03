b9db62036a73a1b17a5f8e84a4509933ece725e9
marronnier31 <marronnier31@gmail.com>
2026-02-03 10:41:04 +0900

    [추가] 자바스크립트에서 변수명명규칙

---
+++ section01/chapter01/chapter04.js
@@ -0,0 +1,22 @@
+//1. 변수(5, 3) let, var 구별
+var no = 27;
+console.log(no);
+//var no = 'abc';
+console.log(no);
+
+let no = 27; 
+let age; 
+age = 30; 
+//let age = 40;  //(x)  
+console.log(age);
+
+//2. 상수
+const birth = 1;
+birth = 'dcjaf';
+console.log(birth);
+
+//3. 변수 명명 규칙
+let $a = 10;
+let _a = 2;
+//let 2a = 3;
+//let for = 4;
