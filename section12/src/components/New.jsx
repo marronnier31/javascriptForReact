2e350039b10f2fa878013feed7608116504f7503
marronnier31 <marronnier31@gmail.com>
2026-02-13 14:21:06 +0900

    [나만의 일기장] 입력기능

--- section12/src/components/New.jsx
+++ section12/src/components/New.jsx
@@ -1,14 +1,24 @@
 import Button from "./Button";
 import Header from "./Header";
+import Editor from "./Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+import { useNavigate } from "react-router-dom";
+
 const New = () =>{
-   const onClickButton = e =>{
-    alert(e.target.innerText);
+  const {onCreate} = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+  //버튼전송기능(onCreate)
+  const onSubmit = input => {
+    onCreate(input.createdDate.getTime(),input.emotionId, input.content);
+    //뒤로가기방지
+    nav('/',{replace:true});
   }
+
  return <>
- <Header title={"Header"} leftChild={<Button text={"LEFT"} type={"POSITIVE"}  onClick={onClickButton}/>} rightChild={<Button text={"RIGHT"} type={"NEGATIVE"}  onClick={onClickButton}/>}/>
-  <h1> New</h1>
+ <Header title={"새 일기 쓰기"} leftChild={<Button text={"< 뒤로가기"} onClick={e=>nav(-1)}/>} />
+  <Editor onSubmit={onSubmit}/>
  </> 
 }
 
 export default New;
-;
